838748489ca4b685e61f931def36f8624f2b56b5
tunisia0011 <82506259+tunisia0011@users.noreply.github.com>
2021-06-23 18:40:46 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -141,3 +141,7 @@
 15 0,12,22 * * * node /scripts/jd_jump.js >> /scripts/logs/jd_jump.log 2>&1
 #京喜牧场
 15 0,12,22 * * * node /scripts/jd_jxmc.js >> /scripts/logs/jd_jxmc.log 2>&1
+#京享值pk
+15 0,6,13,19,21 * * * node /scripts/jd_ddo.js >> /scripts/logs/jd_ddo.log 2>&1
+#点点券
+10 0,20 * * * node /scripts/jd_necklace,js >> /scripts/logs/jd_necklace,log 2>&1
