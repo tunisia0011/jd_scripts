965fcf0c3fb427a8e3b81672cd40b18e2deede5e
tunisia0011 <tunisia0011@outlook.com>
2021-07-29 04:41:06 +0800

    update

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -168,6 +168,8 @@
 59 11,12,23 * * * node /scripts/jd_cfdtx.js >> /scripts/logs/jd_cfdtx.log 2>&1
 # 京喜财富岛互助
 18 0,1,9,14,18 * * * node /scripts/jd_cfd_help.js >> /scripts/logs/jd_cfd_help.log 2>&1
+# 京喜领红包
+4 2,10 * * * node /scripts/jd_jxlhb.js >> /scripts/logs/jd_jxlhb.log 2>&1
 # 删除优惠券(默认注释，如需要自己开启，如有误删，已删除的券可以在回收站中还原，慎用)
 #20 9 * * 6 node /scripts/jd_delCoupon.js >> /scripts/logs/jd_delCoupon.log 2>&1
 #家庭号
