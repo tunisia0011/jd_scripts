3b2e6a4b37de4e460a7f3322e10e4004205e0080
tunisia0011 <tunisia0011@outlook.com>
2021-08-23 21:31:12 +0800

    update

--- getJDCookie.js
+++ getJDCookie.js
@@ -4,7 +4,7 @@
  */
 const $ = new Env('扫码获取京东cookie');
 // const JD_UA = require('./USER_AGENTS').USER_AGENT;
-const JD_UA = `Mozilla/5.0 (iPhone; CPU iPhone OS 13_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 SP-engine/2.14.0 main%2F1.0 baiduboxapp/11.18.0.16 (Baidu; P2 13.3.1) NABar/0.0`;
+const JD_UA = `Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0`;
 // console.info('JD_UA', JD_UA)
 const qrcode = require('qrcode-terminal');
 let s_token, cookies, guid, lsid, lstoken, okl_token, token
